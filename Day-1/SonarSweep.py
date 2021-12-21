c6c47fc07b4186a0c715fc4b0d314b7ee1f5496d
Samantha Noggle <44234583+astruxie@users.noreply.github.com>
2021-12-21 13:37:34 -0500

    I DID IT!!!!!

--- Day-1/SonarSweep.py
+++ Day-1/SonarSweep.py
@@ -2,16 +2,18 @@
 
 # Variables
 count = 0
-prev = None
+prev = 0
 
 # Iterating through each measurement & comapring
 f = open("input.txt", "r")
 for measurement in f:
-  if measurement > prev:
+  if int(measurement) > int(prev):
       # wow, really no ++ operator? wtf snake language
       count = count + 1
   prev = measurement
 
+#adjust -1 because the first compare is always causing 1 increment, but shouldnt
+count = count - 1
 # print out answer
-print("There are " + count + " measurements larger than the previous measurement")
+print("There are " + str(count) + " measurements larger than the previous measurement")
     
